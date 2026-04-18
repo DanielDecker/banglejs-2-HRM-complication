41706281d835be40caee4e8df90904ee961a6d94
DanielDecker <90106468+DanielDecker@users.noreply.github.com>
2026-04-18 21:06:51 +0200

    fix lagreComplication

--- Complication.js
+++ Complication.js
@@ -9,10 +9,12 @@
     g.setFontAlign(0,0);
 
     let bp = Math.round(Bangle.getHealthStatus().bpm||Bangle.getHealthStatus("last").bpm);
+    let textLength = g.stringWidth(bp);
+    let spaceLenth = 6;
     
     if (largeComplication) {
       // move hear rate count left, so overall reading is centered
-      x -= 10;
+      x -= (textLength + spaceLenth);
     }
     
     g.drawString(bp, x,y);
@@ -24,16 +26,17 @@
     }
 
     // move write possition
-    x += g.stringWidth(bp);
+    x += textLength;
     
     let Text = 'bpm';
     if (largeComplication) {
-      x += 3;
+      x += spaceLenth;
     } else {
       // shrink & rotate bpm text
       g.setFontAlign(0, 0, 3);
       g.setFont('Vector', 10);
       x -= 10;
+      y -= 2;
     }
     g.drawString(Text, x,y);
     
