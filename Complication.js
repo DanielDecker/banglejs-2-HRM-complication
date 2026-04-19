7fff82c88371ce705600afa155821a240f292e57
DanielDecker <90106468+DanielDecker@users.noreply.github.com>
2026-04-19 18:03:05 +0200

    fix alignment

--- Complication.js
+++ Complication.js
@@ -6,14 +6,14 @@
     // init font
     g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
     g.setFont('Vector', 18);
-    g.setFontAlign(0,0);
+    g.setFontAlign(-1,0);
 
     let bp = Math.round(Bangle.getHealthStatus().bpm||Bangle.getHealthStatus("last").bpm);
     let textLength = g.stringWidth(bp);
     let spaceLength = 6;
     
     if (largeComplication) {
-      // move hear rate count left, so overall reading is centered
+      // move heart rate count left, so overall reading is centered
       x -= (textLength/2);
     }
     
@@ -30,12 +30,12 @@
     
     let Text = 'bpm';
     if (largeComplication) {
-      x += spaceLength;
+      //x += spaceLength;
     } else {
       // shrink & rotate bpm text
-      g.setFontAlign(0, 0, 3);
+      g.setFontAlign(0, -1, 3);
       g.setFont('Vector', 10);
-      x -= 10;
+      //x -= 10;
       y -= 2;
     }
     g.drawString(Text, x,y);
